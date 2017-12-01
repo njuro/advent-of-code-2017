de061b2e98a9a958ff09b13900b11bd81fad5774
njuro <juraj.noge@gmail.com>
2017-12-01 10:12:55 +0100

    Day 1 - Reverse captcha

---
+++ day1/reverse_captcha.py
@@ -0,0 +1,22 @@
+'''
+https://adventofcode.com/2017/day/1
+'''
+
+
+def reverse_captcha(halfway=False):
+    with open("input.txt", 'r') as input:
+        captcha = input.read().replace('\n', "")
+        result = 0
+        for index, n in enumerate(captcha):
+            shift = len(captcha) / 2 if halfway else 1
+            next = int((index + shift) % len(captcha))
+            if n == captcha[next]:
+                result += int(n)
+
+        return result
+
+
+# Part 1
+print(reverse_captcha())
+# Part 2
+print(reverse_captcha(halfway=True))
