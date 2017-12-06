0582a1976d1503441928993afc7d1143fbeea130
njuro <juraj.noge@gmail.com>
2017-12-06 08:53:00 +0100

    Day 6 - Memory reallocation - better solution

--- day6/reallocation.py
+++ day6/reallocation.py
@@ -3,37 +3,27 @@ https://adventofcode.com/2017/day/6
 '''
 
 
-def reallocation(cycle=False):
+def reallocation():
     with open("input.txt", 'r') as input:
         banks = list(map(int, input.readline().split()))
         size = len(banks)
         seen = [[]]
-        marker = None
         steps = 0
 
-        while True:
+        while banks not in seen:
+            seen.append(banks[:])
             steps += 1
             max_index = banks.index(max(banks))
             max_val = banks[max_index]
             banks[max_index] = 0
             for i in range(1, max_val + 1):
                 banks[(max_index + i) % size] += 1
-            if banks in seen:
-                if cycle:
-                    if not marker:
-                        marker = banks[:]
-                        steps = 0
-                    elif banks == marker:
-                        break
-                else:
-                    break
-            else:
-                seen.append(banks[:])
 
-        return steps
+        return seen, banks
 
 
+cycles, last = reallocation()
 # Part 1
-print(reallocation())
+print(len(cycles))
 # Part 2
-print(reallocation(cycle=True))
+print(len(cycles) - cycles.index(last))
